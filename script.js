22e490200e5b9311aa6c4ff43df7dcba8d955f8c
Aavash Dhami <dhamiakkal21@gmail.com>
2024-11-15 17:26:35 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,540 @@
+let songs = [
+  {
+    title: "Badshah Arijit Singh Soulmate Lyrics Ek Tha Raja",
+    artist: "Unknown Artist",
+    image: "./images/BadshahArijitSinghSoulmate.jpg",
+    url: "./songs/Badshah___Arijit_Singh_-_Soulmate__Lyrics____Ek_Tha_Raja(256k) - Copy.mp3",
+  },
+  {
+    title: "Pehle Bhi Main",
+    artist: "Unknown Artist",
+    image: "./images/animal.jpg",
+    url: "./songs/Pehle Bhi Main.mp3",
+  },
+  {
+    title: "Bhirni Jholama Krishna Akheli Sarswati Lamichhane New Lok Dohori",
+    artist: "Unknown Artist",
+    image: "./images/Bhirniholama.jpg",
+    url: "./songs/Bhirni_Jholama_भिर्नी_झोलामा___Krishna_Akheli___Sarswati_Lamichhane___New_Lok_Dohori_Song_2076(128k).m4a",
+  },
+  {
+    artist: "Unknown Artist",
+    title: "Birsiney Hau Ki The Elements Tuborg Open Sessions",
+    url: "./songs/Birsiney_Hau_Ki_-_The_Elements____Tuborg_Open_Sessions(128k) - Copy.m4a",
+    image: "./images/BirsineyHauKiTheElements.jpg",
+  },
+  {
+    artist: "Unknown Artist",
+    title: "Arjan Vailly Ne",
+    url: "../songs/Arjan Vailly Ne.mp3",
+    image: "./images/animal.jpg",
+  },
+  {
+    artist: "Unknown Artist",
+    title:
+      "Kasari Vanu Lyrics Song Swoopna_Suman Lyrical Audio Lyrical Video Nepali Lyrics Song",
+    url: "../songs/Kasari_Vanu_-_Lyrics_Song____Swoopna_Suman____Lyrical_Audio____Lyrical_Video____Nepali_Lyrics_Song__(128k).mp3",
+    image: "./images/BirsineyHauKiTheElements.jpg",
+  },
+  {
+    artist: "Unknown Artist",
+    title:
+      "Kashmir Main Tu Kanyakumari Lyrical Chennai Express Shahrukh K Deepika P Sunidhi C Arijit S Neeti",
+    url: "../songs/Kashmir_Main_Tu_Kanyakumari_Lyrical__Chennai_Express__Shahrukh_K,_Deepika_P,Sunidhi_C,Arijit_S,Neeti(256k).mp3",
+    image: "/images/kashmirMe.jpg",
+  },
+  {
+    artist: "Unknown Artist",
+    title:
+      "Makhna Drive Sushant Singh Rajput Jacqueline Fernandez Tanishk Bagchi Asees Kaur ",
+    url: "../songs/Makhna_-_Drive__Sushant_Singh_Rajput,_Jacqueline_Fernandez__Tanishk_Bagchi,_Asees_Kaur(256k).mp3",
+    image: "/images/MakhnaSushant.jpg",
+  },
+  {
+    artist: "Sushant KC",
+    title: "Sushant KC Kya Kardiya Official Music Video",
+    url: "../songs/Sushant_KC_-_Kya_Kardiya__Official_Music_Video_(256k).mp3",
+    image: "/images/kyakardia.jpg",
+  },
+  {
+    artist: "Sushant KC",
+    title: "Sushant KC Risaune Bhay",
+    url: "../songs/Sushant_KC_-_Risaune_Bhaye(256k).mp3",
+    image: "/images/risauneBhaye.jpg",
+  },
+  {
+    artist: "Unknown Artist",
+    title:
+      "Phutkera Gayo Umer Sajjan Raj Vaidya original Cover by Saugan Shrestha With Lyric",
+    url: "./songs/Phutkera_Gayo_Umer____Sajjan_Raj_Vaidya__original____Cover_by_Saugan_Shrestha__With_Lyric_(128k).m4a",
+    image: "./images/PhutkeraGayo.jpg",
+  },
+
+  {
+    artist: "Unknown Artist",
+    title:
+      "Ghar Kata Ho Bainiko Launna Dajai Pirati Prakash Dutraj Shanti Shree Pariyar New Lok Dohori",
+    url: "./songs/Ghar_Kata_Ho_Bainiko___Launna_Dajai_Pirati____Prakash_Dutraj___Shanti_Shree_Pariyar___New_Lok_Dohori(128k).m4a",
+    image: "./images/GharKataHoBainikoLaunna.jpg",
+  },
+  {
+    title: "Kalilo tamalai by Padam Bista and Tara Thapa Lyrics Video",
+    artist: "Unknown Artist",
+    url: "./songs/Kalilo_tamalai_by_Padam_Bista_and_Tara_Thapa__Lyrics_Video_(128k).m4a",
+    image: "./images/Kalilotamalai.jpg",
+  },
+  {
+    artist: "Unknown Artist",
+    title:
+      "kanxi meri kanxi malai kanxaa banayer rakhana sajjanrajvaidya lyrics song",
+    url: "./songs/kanxi_meri_kanxi_malai_kanxaa_banayer_rakhana__@sajjanrajvaidya__lyrics_song(128k).m4a",
+    image: "./images/kanximerikanximalaikanxaa.jpg",
+  },
+  {
+    artist: "Unknown Artist",
+    title:
+      "Tera Yaar Hoon Main Lyrics Arijit Singh Rochak Kohli Friendship Day Special",
+    url: "./songs/Tera_Yaar_Hoon_Main__Lyrics__Arijit_Singh___Rochak_Kohli__Friendship_Day_Special_(256k).mp3",
+    image: "./images/TeraYaarHoonMain.jpg",
+  },
+  {
+    artist: "Sushant KC",
+    title: "Sathi Sushant KC Official Music Video",
+    url: "./songs/Sathi_-_Sushant_KC_(Official_Music_Video)(128k).m4a",
+    image: "./images/SathiSushantKC.jpg",
+  },
+  {
+    artist: "Unknown Artist",
+    title: "Sajjan Raj Vaidya Sasto Mutu Official Release",
+    url: "./songs/Sajjan_Raj_Vaidya_-_Sasto_Mutu_[Official_Release](128k).m4a",
+    image: "./images/SajjanRajVaidyaSastoMutu.jpg",
+  },
+  {
+    artist: "Unknown Artist",
+    title: "Sajjan Raj Vaidya Dhairya Official Release",
+    url: "./songs/Sajjan_Raj_Vaidya_-_Dhairya_[Official_Release](128k).m4a",
+    image: "./images/SajjanRajVaidyaDhairya.jpg",
+  },
+  {
+    artist: "Unknown Artist",
+    title: "Resham firiri famous Nepali song",
+    url: "./songs/Resham_firiri_famous_Nepali_song(256k).mp3",
+    image: "./images/Reshamfiriri.jpg",
+  },
+  {
+    artist: "Unknown Artist",
+    title:
+      "New Nepali lyrics remix song Timi bhanda ma paila maraula futkerw gayo Mero manaiko",
+    url: "./songs/New_Nepali_lyrics_remix_song_Timi_bhanda_ma_paila_maraula____futkerw_gayo__Mero_manaiko...(128k).m4a",
+    image: "./images/Timi_bhanda_ma_paila_maraula.jpg",
+  },
+  {
+    artist: "Unknown Artist",
+    title: "Ma Tira Farki Herana Karan bhatta Official lyrical video Prod",
+    url: "./songs/Ma_Tira_Farki_Herana___Karan_bhatta___Official_lyrical_video___Prod._Anup_kunwar(128k).m4a",
+    image: "./images/MaTiraFarkiHerana.jpg",
+  },
+
+  {
+    artist: "Unknown Artist",
+    title: "PAISA Seven Hundred Fifty Official song kushal pokhrel",
+    url: "./songs/PAISA_-_Seven_Hundred_Fifty__Official_song__-_kushal_pokhrel(256k).mp3",
+    image: "/images/paisa.jpg",
+  },
+  {
+    artist: "Unknown Artist",
+    title: "PAISA 2.0  Official song kushal pokhrel",
+    url: "./songs//Paisa_2.0____Ninety_Nine_____Official_song_2023_____Kushal_Pokhrel(256k).mp3",
+    image: "/images/paisa2.0.jpg",
+  },
+  {
+    artist: "Unknown Artist",
+    title: "Sajjan Raj Vaidya Naganya Maya Official Releas",
+    url: "./songs/Sajjan_Raj_Vaidya_-_Naganya_Maya_[Official_Release](128k).m4a",
+    image: "/images/naganyaMaya.jpg",
+  },
+  {
+    artist: "Unknown Artist",
+    title:
+      "Sora barse umeraima old is gold Evergreen nepali song Aruna lama Rudramani gurung",
+    url: "./songs/Sora_barse_umeraima-_old_is_gold__Evergreen_nepali_song___Aruna_lama__Rudramani_gurung__(256k).mp3",
+    image: "/images/soraBarsa.jpg",
+  },
+  {
+    artist: "Unknown Artist",
+    title: "Phool Pati Bhakera Manakamana Bharosha Movie Song ",
+    url: "/songs/Phool_Pati_Bhakera_Manakamana____Bharosha_Movie_Song___(256k).mp3",
+    image: "/images/phoolPati.jpg",
+  },
+  {
+    artist: "Sushant Kc",
+    title: "Sushant Kc Bardali Ft",
+    url: "/songs/Sushant_KC_-_Bardali_ft._Indrakala_Rai__Official_Music_Video_(256k).mp3",
+    image: "/images/bardali.jpg",
+  },
+  {
+    artist: "Unknown Artist",
+    title:
+      "I Love You Santaram Wilson Bikram Rai Nepali Comedy Song Takme Buda",
+    url: "/songs/I_Love_You_Santaram_-_Wilson_Bikram_Rai__Nepali_Comedy_Song__Takme_Buda___Manik_Mishravevo_Official(256k).mp3",
+    image: "/images/santaram.jpg",
+  },
+  {
+    artist: "Sushant Kc",
+    title: "Sushant Kc Parkha Na",
+    url: "/songs/Sushant_KC_-_Parkha_Na_ft._Jhuma_Limbu__Official_Music_Video_(256k).mp3",
+    image: "/images/parkhana.jpg",
+  },
+];
+
+
+
+let color = document.querySelector(".song-image img");
+
+let currentSongIndex = 0;
+let isPlaying = false;
+let shuffleMode = false;
+
+// DOM elements
+const songTitle = document.getElementById("songTitle");
+const artistName = document.getElementById("artistName");
+const songImage = document.getElementById("songImage");
+const playButton = document.getElementById("play");
+const shuffleButton = document.getElementById("shuffle-btn");
+const progressBar = document.getElementById("progress");
+const audio = new Audio();
+
+let isLooping = false;
+
+const loopButton = document.getElementById("loop-btn");
+const loopIcon = document.getElementById("loop-icon");
+
+const hamburger = document.getElementById("hamburger");
+const songListContainer = document.getElementById("songListContainer");
+const songList = document.getElementById("songList");
+const audioPlayer = document.getElementById("audioPlayer");
+function extractAndFormatSongName(url) {
+  const songName = url.split("/").pop().split(".")[0];
+  const cleanedName = songName
+    .replace(/[^a-zA-Z\s]/g, " ")
+    .replace(/\s+/g, " ")
+    .trim()
+    .toLowerCase()
+    .replace(/\b\w/g, (c) => c.toUpperCase());
+
+  return cleanedName;
+}
+
+// songs.forEach((i) => {
+//   console.log(extractAndFormatSongName(i.url));
+// });
+
+function playSong() {
+  isPlaying = true;
+  playButton.innerHTML = `<i class="ri-pause-line"></i>`;
+  audio.play();
+}
+
+function pauseSong() {
+  isPlaying = false;
+  playButton.innerHTML = `<i class="ri-play-fill"></i>`;
+  audio.pause();
+}
+
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+songs = songs.map((song) => {
+  return { ...song, color: getRandomColor() };
+});
+
+console.log(songs);
+
+
+setInterval(() => {
+  color.style.boxShadow = `0px 0px 14px 3px  ${songs[currentSongIndex].color}`;
+  
+}, 100);
+// setInterval(getRandomColor, 1000);
+
+
+
+
+
+
+let volumeControl = document.querySelector("#volume-control");
+volumeControl.addEventListener("input", function () {
+  audio.volume = volumeControl.value;
+});
+
+playButton.addEventListener("click", () => {
+  isPlaying ? pauseSong() : playSong();
+  isPlaying
+    ? showDescription("play-song-toast")
+    : showDescription("pause-song-toast");
+});
+
+document.getElementById("forward").addEventListener("click", nextSong);
+
+
+
+function nextSong() {
+  if (shuffleMode) {
+    currentSongIndex = Math.floor(Math.random() * songs.length);
+  } else {
+    currentSongIndex = (currentSongIndex + 1) % songs.length;
+  }
+  loadSong(songs[currentSongIndex]);
+  playSong();
+  updatePlayingClass();
+}
+function forago() {
+  audio.currentTime -= 5;
+  updatePlayingClass();
+}
+function backago() {
+  audio.currentTime += 10;
+  updatePlayingClass();
+}
+document.getElementById("backward").addEventListener("click", () => {
+  if (currentSongIndex > 0) {
+    currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+  }
+  loadSong(songs[currentSongIndex]);
+  playSong();
+  updatePlayingClass();
+});
+
+
+shuffleButton.addEventListener("click", () => {
+  shuffleMode = !shuffleMode;
+  if (shuffleMode) {
+    shuffleButton.style.color = "#00ff99";
+    songs = shuffleArray(songs);
+    showDescription("shuffle-desc");
+  } else {
+    shuffleButton.style.color = "#888888";
+    showDescription("shuffle-desc-off");
+    // shuffleButton.classList.remove("shuffle-active");
+    songs.sort((a, b) => {
+      return a.title.localeCompare(b.title);
+    });
+  }
+});
+
+function showDescription(descId) {
+  const description = document.getElementById(descId);
+  description.style.display = "block";
+  description.style.opacity = "1";
+
+  setTimeout(() => {
+    description.style.display = "none";
+    description.style.opacity = "1";
+  }, 1300);
+}
+
+function shuffleArray(songs) {
+  for (let i = songs.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [songs[i], songs[j]] = [songs[j], songs[i]];
+  }
+
+  return songs;
+}
+
+function updateOpacity() {
+  // Disable backward button if at the first song
+  document.getElementById("backward").style.opacity =
+    currentSongIndex === 0 ? 0.4 : 1;
+  document.getElementById("backward").style.cursor =
+    currentSongIndex === 0 ? "no-drop" : "pointer";
+
+  // Disable forward button if at the last song
+  document.getElementById("forward").style.opacity =
+    currentSongIndex === songs.length - 1 ? 0.4 : 1;
+  document.getElementById("forward").style.cursor =
+    currentSongIndex === songs.length - 1 ? "no-drop" : "pointer";
+}
+
+// updateOpacity()
+// Toggle loop functionality
+loopButton.addEventListener("click", () => {
+  isLooping = !isLooping;
+  audio.loop = isLooping;
+
+  if (isLooping) {
+    loopButton.classList.add("active");
+    loopButton.innerHTML = '<i class="ri-repeat-one-line"></i>';
+    showDescription("loop-desc");
+  } else {
+    loopButton.classList.remove("active");
+    loopButton.innerHTML = '<i class="ri-repeat-2-fill"></i>';
+    showDescription("single-loop-desc");
+  }
+});
+
+// Handle song end (same as before)
+audio.addEventListener("ended", () => {
+  if (!isLooping) {
+    nextSong();
+  } else {
+    playSong();
+  }
+});
+
+// Load the first song initially
+loadSong(songs[currentSongIndex]);
+
+const progressBarContainer = document.getElementById("progress-bar");
+
+audio.addEventListener("timeupdate", updateProgressBar);
+function updateProgressBar() {
+  const progressPercent = (audio.currentTime / audio.duration) * 100;
+  progressBar.style.width = `${progressPercent}%`;
+
+  // Update starting time and ending time
+  document.getElementById("starting-time").textContent = formatTime(
+    audio.currentTime
+  );
+  document.getElementById("ending-time").textContent = formatTime(
+    audio.duration
+  );
+}
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+  return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+}
+
+progressBarContainer.addEventListener("click", (e) => {
+  const width = progressBarContainer.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+  const newTime = (clickX / width) * duration;
+
+  audio.currentTime = newTime;
+  updateProgressBar();
+
+  playSong();
+});
+
+
+function loadSong(song) {
+  songTitle.innerHTML = song.title;
+  artistName.innerHTML = song.artist;
+  songImage.src = song.image;
+  audio.src = song.url;
+
+  progressBar.style.width = "0%";
+  document.getElementById("starting-time").textContent = "00:00";
+  document.getElementById("ending-time").textContent = formatTime(
+    audio.duration
+  );
+}
+loadSong(songs[currentSongIndex]);
+
+// Function to update the .playing class
+function updatePlayingClass() {
+  document
+    .querySelectorAll(".song-item")
+    .forEach((item) => item.classList.remove("playing"));
+  document
+    .querySelectorAll(".song-item")
+    [currentSongIndex].classList.add("playing");
+}
+
+
+// Display song list
+function displaySongList() {
+  songList.innerHTML = "";
+  songs.forEach((song, index) => {
+    const li = document.createElement("li");
+    li.classList.add("list-group-item", "song-item");
+    li.textContent = song.title;
+
+    // Create play icon
+    const playIcon = document.createElement("i");
+    playIcon.classList.add("ri-play-circle-line", "play-icon");
+
+    // Create sound bars container
+    const barsContainer = document.createElement("div");
+    barsContainer.classList.add("sound-bars");
+
+    // Create individual bars inside sound bars container
+    for (let i = 0; i < 3; i++) {
+      const bar = document.createElement("div");
+      bar.classList.add("bar");
+      barsContainer.appendChild(bar);
+    }
+
+    // Append play icon and sound bars to the li
+    li.appendChild(playIcon);
+    li.appendChild(barsContainer);
+
+    // Click event to play song and show bars
+    li.addEventListener("click", () => {
+      currentSongIndex = index;
+      loadSong(song);
+      playSong();
+      updatePlayingClass();
+    });
+
+    songList.appendChild(li);
+  });
+}
+
+displaySongList();
+
+hamburger.addEventListener("click", () => {
+  songListContainer.style.display =
+    songListContainer.style.display === "block" ? "none" : "block";
+});
+document.getElementById("removeSong").addEventListener("click", () => {
+  songListContainer.style.display = "none";
+});
+
+// JavaScript: Add download functionality
+document.getElementById("downloadBtn").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Add your download functionality here
+  // Example:
+  downloadSong(songs[currentSongIndex].url, songs[currentSongIndex].title);
+});
+
+function downloadSong(url, title) {
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = title;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+// Handle playback speed button click
+document.getElementById("playSpeedBtn").addEventListener("click", () => {
+  // Show/hide speed options dropdown
+
+  const speedOptions = document.getElementById("playbackSpeedOptions");
+
+  speedOptions.style.display =
+    speedOptions.style.display === "none" ? "block" : "none";
+});
+
+document
+  .querySelectorAll(".playback-speed-options button")
+  .forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const speed = event.target.getAttribute("data-speed");
+      audio.playbackRate = parseFloat(speed);
+      console.log(`Playback speed set to: ${speed}x`);
+      document.getElementById("playbackSpeedOptions").style.display = "none";
+    });
+  });
